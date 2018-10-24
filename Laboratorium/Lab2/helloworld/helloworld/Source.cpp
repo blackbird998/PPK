d1a21b513e2edecb46f1ca826151813699e4fcbd
AS <blackbird998@protonmail.com>
2018-10-24 19:53:09 +0200

    Funkcje

--- Laboratorium/Lab2/helloworld/helloworld/Source.cpp
+++ Laboratorium/Lab2/helloworld/helloworld/Source.cpp
@@ -1,31 +1,41 @@
 #include <iostream>
-#include <math.h>
+//#include <math.h>
 using namespace std;
 
-
+int minmax();
+float rownanie_kwadratowe();
+float kalkulator();
 
 int main(){
+
+	//minmax();
+	rownanie_kwadratowe();
+	//kalkulator();
+
+	return 0;
+}
+
+int minmax() {
 	int wartosc = 0;
 	cout << "Podaj liczbe calkowita" << endl;
 	cin >> wartosc;
-	int max= wartosc;
-	int min= wartosc;
-	
+	int max = wartosc;
+	int min = wartosc;
+
 
 	do {
 		cout << "Podaj liczbe calkowita" << endl;
 		cin >> wartosc;
 		if (wartosc > max) max = wartosc;
 		if (wartosc < min) min = wartosc;
-		
+	} while (wartosc != pow(2, 15));
+	cout << "Min=" << min << ", " << "Max=" << max << endl;
 
+	return 0;
+}
 
-		
-	} while (wartosc !=  pow(2, 15));
-	cout << "Min="<< min << ", " << "Max="<< max << endl;
+float rownanie_kwadratowe() {
 
-/*
-//równanie kwadratowe
 	cout << "Podaj a: ";
 	float a = 0;
 	cin >> a;
@@ -36,12 +46,12 @@ int main(){
 	float c = 0;
 	cin >> c;
 
-	float delta = b*b - 4 * a*c;
+	float delta = b * b - 4 * a*c;
 	//cout << "Delta jest rowna: " << delta << endl;
 	cout << a << "x^2+" << b << "x" << "+" << c << endl;
 	if (a == 0) {
 		cout << "Rownanie nie jest rownaniem kwadratowym!" << endl;
-	}	
+	}
 	else {
 		if (delta < 0) {
 			cout << "Delta jest ujemna, brak pierwiastkow" << endl;
@@ -54,26 +64,27 @@ int main(){
 			else {
 				float x1 = (-b - sqrt(delta)) / (2 * a);
 				float x2 = (-b + sqrt(delta)) / (2 * a);
-				cout << "x1=" << x1 <<", "<< "x2=" << x2 << endl;
+				cout << "x1=" << x1 << ", " << "x2=" << x2 << endl;
 			}
-			
+
 		}
 	}
+	return 0;
+}
 
-*/
+float kalkulator() {
+	float a = 0;
+	float b = 0;
+	cout << "Podaj a i b: " << endl;
+	cin >> a;
+	cin >> b;
 
-/*
-//kalkulator
 	cout << "Suma a i b to: " << a + b << endl;
 	cout << "Roznica a i b to: " << a - b << endl;
 	cout << "Iloczyn a i b to: " << a * b << endl;
 	if (b != 0)
 		cout << "Iloraz a i b to: " << a / b << endl;
 	else
-		cout << "Nie mo¿na dzieliæ przez zero!" << endl;
-*/
-	
-
+		cout << "Nie mo¿na dzieliEprzez zero!" << endl;
 	return 0;
 }
-
